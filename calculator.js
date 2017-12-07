30c63c7f2bb3bfe1fd5f70e752344301d4dbb9e1
Varun Vemulapalli <varun.vemulapalli@warriorlife.net>
2017-12-07 11:29:35 -0800

    Finished Javascript

--- calculator.js
+++ calculator.js
@@ -2,4 +2,20 @@
 
 // Change Display
 function d(val) {
-	document.getElementById("d").value
+	document.getElementById("d").value = val;
+}
+
+// Type numbers and operators
+function v(val) {
+	document.getElementById("d").value += val;
+}
+
+// Evaluate the Equation
+function e() {
+	try {
+		d(eval(document.getElementById("d") value));
+	}
+	catch(err) {
+		d("Error");
+	}
+}
